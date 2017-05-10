582db855eae4490b9bab2510f21681aa110c4152
stepango <step.89.g@gmail.com>
2017-05-10 14:49:21 +0800

    Annotations converted to kotlin

--- common/src/main/java/io/reactivex/common/annotations/CheckReturnValue.kt
+++ common/src/main/java/io/reactivex/common/annotations/CheckReturnValue.kt
@@ -1,33 +1,24 @@
 /**
  * Copyright (c) 2016-present, RxJava Contributors.
- *
+
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- *
+
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
  * the License for the specific language governing permissions and limitations under the License.
  */
 
-package io.reactivex.common.annotations;
-
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+package io.reactivex.common.annotations
 
 /**
  * Marks methods whose return values should be checked.
- *
  * @since 2.0.2 - experimental
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-@Target(ElementType.METHOD)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Experimental
-public @interface CheckReturnValue {
-
-}
+annotation class CheckReturnValue
--- common/src/main/java/io/reactivex/common/annotations/NonNull.kt
+++ common/src/main/java/io/reactivex/common/annotations/NonNull.kt
@@ -1,33 +1,23 @@
 /**
  * Copyright (c) 2016-present, RxJava Contributors.
- *
+
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- *
+
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
  * the License for the specific language governing permissions and limitations under the License.
  */
 
-package io.reactivex.common.annotations;
-
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+package io.reactivex.common.annotations
 
 /**
  * Indicates that a field/parameter/variable/return type is never null.
  */
-@Documented
-@Target(value = {FIELD, METHOD, PARAMETER, LOCAL_VARIABLE})
-@Retention(value = CLASS)
-public @interface NonNull { }
+@MustBeDocumented
+@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
+@Retention(AnnotationRetention.SOURCE)
+annotation class NonNull
 
